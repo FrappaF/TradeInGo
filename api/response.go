9a37b5381d20ce1dd714b7139e4092117488a5b0
Francesco <34452508+FrappaF@users.noreply.github.com>
2022-10-09 16:01:03 +0200

    Hidden private token

--- api/response.go
+++ api/response.go
@@ -2,14 +2,15 @@ package api
 
 import (
 	"context"
+	"os"
 
 	finnhub "github.com/Finnhub-Stock-API/finnhub-go/v2"
 )
 
-//Initialize a new finnhub configuration and retrieve a finnhub.Cryptocandles for the given symbol, resolution, from and to
+// Initialize a new finnhub configuration and retrieve a finnhub.Cryptocandles for the given symbol, resolution, from and to
 func GetResponse(symbol, resolution string, from, to int64) (finnhub.CryptoCandles, error) {
 	cfg := finnhub.NewConfiguration()
-	cfg.AddDefaultHeader("X-Finnhub-Token", "cbrv4g2ad3idk2bn0180")
+	cfg.AddDefaultHeader("X-Finnhub-Token", os.Getenv("TOKEN"))
 	finnhubClient := finnhub.NewAPIClient(cfg).DefaultApi
 
 	res, _, err := finnhubClient.CryptoCandles(context.Background()).Symbol(symbol).Resolution(resolution).From(from).To(to).Execute()
